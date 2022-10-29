96cdd0afbedd3f63a89ad3848820eda464872660
Sang il Park <sangil6372@naver.com>
2022-10-29 22:49:54 +0900

    b2587

---
+++ baekJoonJava/src/b2587/Main.java
@@ -0,0 +1,25 @@
+package b2587;
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		
+		int[] list = new int[5];
+		int sum = 0;
+		
+		for(int i =0;i<5;i++) {
+			list[i] = sc.nextInt();
+			sum+=list[i];
+		}
+		
+		Arrays.sort(list);
+		System.out.println(sum/5 + " " + list[2]);
+
+		sc.close();
+	}
+}
