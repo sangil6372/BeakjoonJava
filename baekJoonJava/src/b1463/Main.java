c4868dc2c02a8492a2481d4e4e21887f13890899
Sang il Park <sangil6372@naver.com>
2022-11-30 23:12:36 +0900

    b1463 문제의 조건을 제대로 읽기

--- baekJoonJava/src/b1463/Main.java
+++ baekJoonJava/src/b1463/Main.java
@@ -1,26 +1,26 @@
 package b1463;
 
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
 	static Integer[] dp;
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
+		int N = Integer.parseInt(st.nextToken());
 		
 		
 		dp = new Integer[N+1];
 		dp[0] = dp[1] = 0;
-		dp[2] = dp[3] = 1;
-		
 		
 		System.out.println(recur(N));
-		sc.close();
 	}
 	
 	
