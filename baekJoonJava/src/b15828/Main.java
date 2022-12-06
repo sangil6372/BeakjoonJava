78853f0a55d3be778ba9a79818add5ceb7456422
Sang il Park <sangil6372@naver.com>
2022-12-06 19:40:33 +0900

    .

--- baekJoonJava/src/b15828/Main.java
+++ baekJoonJava/src/b15828/Main.java
@@ -11,29 +11,32 @@ public class Main {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		
+
 		int N = Integer.parseInt(br.readLine());
-		
+
 		Queue<Integer> queue = new LinkedList<>();
-		
-		while(true) {
+
+		while (true) {
 			int buffer = Integer.parseInt(br.readLine());
-			if (buffer == -1) break;
-			else if (buffer > 0 ) {
-				if (queue.size()<=N) {
-					queue.add(buffer);
-				} continue;
-			}
-			else if (buffer == 0) {
-				queue.poll();
-			}
+			if (buffer == -1)
+				break;
+			else if (buffer == 0)
+				queue.remove();
+			else if (queue.size() < N)
+				queue.add(buffer);
+
 		}
-		for (int i : queue) {
-			sb.append(i + " ");
+		
+		if (queue.isEmpty())
+			System.out.println("empty");
+		else {
+			for (int i : queue) {
+
+				sb.append(i + " ");
+			}
+			System.out.println(sb);
 		}
-		System.out.println(sb);
-		
-		
+
 	}
 
 }
