482f6a9879c1c9455817a042894eebed948852a8
Sang il Park <sangil6372@naver.com>
2022-12-19 23:23:32 +0900

    dp 설정하기 어려운 문제

--- baekJoonJava/src/dp/b11054/Main.java
+++ baekJoonJava/src/dp/b11054/Main.java
@@ -3,34 +3,70 @@ package dp.b11054;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	public static int[] dp;
+	public static int[] dpA;
+	public static int[] dpB;
+	public static int[] arr;
+	public static int N;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		N = Integer.parseInt(br.readLine());
+
+		arr = new int[N + 1];
+		dpA = new int[N + 1];
+		dpB = new int[N + 1];
+		int dp[] = new int [N+1];
 		
-		int N = Integer.parseInt(br.readLine());
-
-		int[] arr = new int[N + 1];
-		dp = new int[N + 1];
-
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		for (int i = 1; i <= N; i++) {
+
+		for (int i = 0; i < N; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
-			
-			
+
 		}
+		
+		int max = 1;
+		
+		for (int i = 0; i < N; i++) {
+			dp[i] = ascendingSeq(i) + descendingSeq(i) -1;
+			if (dp[i]>max) max = dp[i];
+		}
+		
+		System.out.println(max);
+		
 
 	}
-	
-	public static int bitonicSeq(int n) {
-		
-		
+
+	public static int ascendingSeq(int n) {
+		if (dpA[n] == 0) {
+			dpA[n] = 1;
+
+			for (int i = n - 1; i >= 0; i--) {
+				if (arr[n] > arr[i]) {
+					dpA[n] = Math.max(dpA[n], ascendingSeq(i) + 1);
+				}
+			}
+		}
+		return dpA[n];
+
 	}
-	
+
+	public static int descendingSeq(int n) {
+		if (dpB[n] == 0) {
+			dpB[n] = 1;
+
+			for (int i = n + 1; i < N; i++) {
+				if (arr[n] > arr[i]) {
+					dpB[n] = Math.max(dpB[n], descendingSeq(i) + 1);
+				}
+			}
+		}
+		return dpB[n];
+
+	}
+
 }
