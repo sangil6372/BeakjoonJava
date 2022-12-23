5ad4505ae2f012e3483b5908feac50f98915df72
Sang il Park <sangil6372@naver.com>
2022-12-24 00:22:39 +0900

    dfs, bfs

--- baekJoonJava/src/graph/b1260/Main.java
+++ baekJoonJava/src/graph/b1260/Main.java
@@ -1,24 +1,26 @@
-package graph.b24444;
+package graph.b1260;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-
 	static int N;
 	static int[] visited;
+	static int[] visited2;
 	static ArrayList<ArrayList<Integer>> graph;
-	static int[] vertexOrder;
-	static int count = 1;
+	static int[] vertexOrderDfs;
+	static int[] vertexOrderBfs;
 
-	public static void main(String[] args) throws IOException {
+	static int count = 1;
+	static StringBuilder sb;
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
@@ -28,7 +30,8 @@ public class Main {
 
 		graph = new ArrayList<>();
 		visited = new int[N + 1];
-		vertexOrder = new int[N + 1];
+		visited2 = new int[N + 1];
+
 
 		for (int i = 0; i <= N; i++) {
 			graph.add(new ArrayList<>());
@@ -44,43 +47,52 @@ public class Main {
 		}
 
 		for (int i = 1; i <= N; i++) {
-			Collections.sort(graph.get(i), new Comparator<Integer>() {
-
-				@Override
-				public int compare(Integer o1, Integer o2) {
-					return o2 - o1;
-				}
-			});
+			Collections.sort(graph.get(i));
 		}
 
+		sb = new StringBuilder();
+		
+		dfs(R);
+		
+		sb.append("\n");
+		count = 1;
+		
 		bfs(R);
+		System.out.println(sb);
+		
+	}
 
-		for (int i = 1; i <= N; i++) {
-			System.out.println(vertexOrder[i]);
+	public static void dfs(int v) {
+		visited[v] = 1;
+		sb.append(v+" ");
+
+		for (int vertex : graph.get(v)) {
+			if (visited[vertex] == 0) {
+				dfs(vertex);
+			}
+
 		}
-
 	}
 
 	public static void bfs(int v) {
 
 		Queue<Integer> queue = new LinkedList<>();
 		queue.add(v);
-		visited[v] = 1;
+		visited2[v] = 1;
 
 		while (!queue.isEmpty()) {
 
 			int peek = queue.poll();
-			vertexOrder[peek] = count++;
+			sb.append(peek + " ");
 
 			for (int vertex : graph.get(peek)) {
-				if (visited[vertex] == 0) {
+				if (visited2[vertex] == 0) {
 					queue.add(vertex);
-					visited[vertex] = 1;
+					visited2[vertex] = 1;
 				}
 			}
 
 		}
 
 	}
-
 }
--- baekJoonJava/src/graph/b2606/Main.java
+++ baekJoonJava/src/graph/b2606/Main.java
@@ -10,25 +10,24 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	
+
 	static int N;
 	static int[] visited;
 	static ArrayList<ArrayList<Integer>> graph;
-	
+
 	static int count = 0;
-	
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
 
 		N = Integer.parseInt(br.readLine());
 		int M = Integer.parseInt(br.readLine());
-		
+
 		int R = 1;
-		
+
 		graph = new ArrayList<>();
-		visited = new int[N+1];
-		
+		visited = new int[N + 1];
+
 		for (int i = 0; i <= N; i++) {
 			graph.add(new ArrayList<>());
 		}
@@ -41,49 +40,38 @@ public class Main {
 			graph.get(v1).add(v2);
 			graph.get(v2).add(v1);
 		}
-		
+
 		for (int i = 1; i <= N; i++) {
 			Collections.sort(graph.get(i));
 
-			
 		}
-		
-		bfs(R);
-		
 
-		
+		bfs(R);
+
 		System.out.println(count);
-		
+
 	}
-	
+
 	public static void bfs(int v) {
-		
+
 		Queue<Integer> queue = new LinkedList<>();
 		queue.add(v);
 		visited[v] = 1;
-		
+
 		while (!queue.isEmpty()) {
-			
+
 			int peek = queue.poll();
-			
-			
-			for(int vertex : graph.get(peek)) {
-				if (visited[vertex]==0) {
+
+			for (int vertex : graph.get(peek)) {
+				if (visited[vertex] == 0) {
 					queue.add(vertex);
 					visited[vertex] = 1;
 					count++;
 				}
 			}
-			
-			
-			
-			
+
 		}
-		
-		
-		
-		
-		
+
 	}
 
 }
