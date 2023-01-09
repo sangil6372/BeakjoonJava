531288739057eff2378eaf739e9e5969a54e877e
Sang il Park <sangil6372@naver.com>
2023-01-09 14:08:41 +0900

    bfs - example

--- baekJoonJava/src/dijkstra/b13549/Main.java
+++ baekJoonJava/src/dijkstra/b13549/Main.java
@@ -1,32 +1,73 @@
 package dijkstra.b13549;
 
-import java.util.Arrays;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Main {
 
-	static int[] dist;
-	final static int INF = 200000000;
+	static int[] visited;
+	static int[] arr;
+	static int K;
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+
 		int N = sc.nextInt();
-		int K = sc.nextInt();
-		
-		dist = new int[100001];
-		Arrays.fill(dist, INF);
-		
-		dist[N] = 0;
-		
+		K = sc.nextInt();
+
+		arr = new int[100001];
+		visited = new int[100001];
+
+		if (N == K) {
+			System.out.println("0");
+		} else {
+
+			bfs(N);
+			System.out.println(arr[K]);
+		}
 		sc.close();
 	}
 
-	public static void dijkstra() {
-		
-		
-		
-		
+	public static void bfs(int start) {
+
+		Deque<Integer> queue = new LinkedList<>();
+		visited[start] = 0;
+		queue.addFirst(start);
+
+		while (!queue.isEmpty()) {
+
+			int cur = queue.pollFirst();
+
+			if (arr[K] != 0)
+				return;
+
+			if (cur * 2 >= 0 && cur * 2 <= 100000) {
+				if (visited[2 * cur] == 0) {
+					visited[2 * cur]++;
+					arr[2 * cur] = arr[cur] ;
+					queue.addFirst(2 * cur);
+				}
+			}
+			
+			if (cur - 1 >= 0 && cur - 1 <= 100000) {
+				if (visited[cur - 1] == 0) {
+					visited[cur - 1]++;
+					arr[cur - 1] = arr[cur] + 1;
+					queue.addLast(cur - 1);
+				}
+			}
+
+			if (cur + 1 >= 0 && cur + 1 <= 100000) {
+				if (visited[cur + 1] == 0) {
+					visited[cur + 1]++;
+					arr[cur + 1] = arr[cur] + 1;
+					queue.addLast(cur + 1);
+				}
+			}
+
+		}
+
 	}
-	
+
 }
