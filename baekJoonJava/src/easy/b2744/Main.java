962b0713116863562e7ac708a91232bb6858f55e
Sang il Park <sangil6372@naver.com>
2023-01-11 23:12:41 +0900

    b5

---
+++ baekJoonJava/src/easy/b2744/Main.java
@@ -0,0 +1,25 @@
+package easy.b2744;
+
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		StringBuilder sb = new StringBuilder();
+		
+		String str = sc.next();
+		
+		for (int i = 0; i<str.length();i++) {
+			char c = str.charAt(i);
+			if ('a'<=c&&c<='z')
+				sb.append((char)(c-'a'+'A'));
+			else if ('A'<=c&&c<='Z')
+				sb.append((char)(c-'A'+'a'));
+		}
+		System.out.println(sb);
+		
+	}
+
+}
