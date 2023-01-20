592dc7e810e51873ac79889d8392421b0b4bebd9
Sang il Park <sangil6372@naver.com>
2023-01-20 14:49:17 +0900

    tree 지름 복습

--- baekJoonJava/src/tree/b1967/Main.java
+++ baekJoonJava/src/tree/b1967/Main.java
@@ -1,101 +1,84 @@
-package tree.b1167;
+package tree.b1967;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
+
 public class Main {
 
-	static ArrayList<ArrayList<Node>> nodes;
-	static int[] visited;
-	static int INF = 9999999;
-	static int max =0;
 	static int N;
+	static int[] visited;
+	static int max = 0;
 	static int maxNode;
 	
 	
-	public static void main(String[] args) throws Exception {
+	static ArrayList<ArrayList<Node>> nodes;
 
+	
+	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		N = Integer.parseInt(br.readLine());
 		
-		// 노드와 
 		nodes = new ArrayList<>();
-
-		// 노드와
-		for (int i = 0; i <= N; i++) {
+		for (int i =0;i<=N;i++) {
 			nodes.add(new ArrayList<>());
 		}
 		
-		// N줄에 걸쳐서 입력 받음 
-		for (int i = 0; i < N; i++) {
+		for (int i = 0;i<N-1;i++) {
 			st = new StringTokenizer(br.readLine());
-			// 첫 숫자 버리기
 			int start = Integer.parseInt(st.nextToken());
-			while (true) {
-				int end = Integer.parseInt(st.nextToken());
-				if (end == -1)
-					break;
-				int weight = Integer.parseInt(st.nextToken());
-
-				nodes.get(start).add(new Node(end, weight));   
-			}
+			int end = Integer.parseInt(st.nextToken());
+			int weight = Integer.parseInt(st.nextToken());
+			
+			nodes.get(start).add(new Node(end,weight));
+			nodes.get(end).add(new Node(start,weight));
+			
 		}
-		// 그래프 채워넣음
-		
-		
-		
-	
-		
-		
+		// 그래프 채워넣기
 		
 		visited = new int[N+1];
 		dfs(1,0);
 		
 		visited = new int[N+1];
 		dfs(maxNode,0);
-		
 		System.out.println(max);
 		
 		
+		
 	}
-	
-	// 다음 노드의 숫자가 값을 가지고 있는 노드 클래스 
-	public static class Node {
+	public static class Node{
 		int end;
 		int weight;
-
+		
 		public Node(int end, int weight) {
 			this.end = end;
 			this.weight = weight;
 		}
-
+		
+		
 	}
-
+	
+	
 	public static void dfs(int v, int len) {
 		visited[v] = 1;
 		
-		if (len>max) {
+		if (max<len) {
 			max = len;
 			maxNode = v;
 		}
 		
-			
 		for (Node n : nodes.get(v)) {
-			
 			if (visited[n.end]==0) {
-				len += n.weight;
-				
-				dfs(n.end, len);
-				
-				len-=n.weight;
+				dfs(n.end, len+n.weight);
 			}
-			
-			
 		}
-		return ;
+		
+		
+		
+		return;
 	}
 
 }
