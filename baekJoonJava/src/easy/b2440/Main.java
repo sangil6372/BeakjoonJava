6973135d7dbb185400d87f46615a5a977a1c3141
Sang il Park <sangil6372@naver.com>
2023-02-13 01:30:51 +0900

    .

---
+++ baekJoonJava/src/easy/b2440/Main.java
@@ -0,0 +1,22 @@
+package easy.b2440;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
+
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N-i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		sc.close();
+	}
+	
+}
