d5b000fe81c19f5bd3b19dc1307eb3999f0f3d31
Sang il Park <sangil6372@naver.com>
2023-02-16 00:32:35 +0900

    .

---
+++ baekJoonJava/src/easy/b2083/Main.java
@@ -0,0 +1,27 @@
+package easy.b2083;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String name = sc.next();
+		int age = sc.nextInt();
+		int weight = sc.nextInt();
+		
+		while(!name.equals("#")||age!=0||weight!=0) {
+			System.out.print(name+ " ");
+			if (age>17||weight>=80) {
+				System.out.println("Senior");
+			}else {
+				System.out.println("Junior");
+			}
+			name =sc.next();
+			age = sc.nextInt();
+			weight = sc.nextInt();
+		}
+	}
+
+}
